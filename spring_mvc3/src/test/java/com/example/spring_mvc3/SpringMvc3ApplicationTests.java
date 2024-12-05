55f286b9f9fc011b67d930e5e2e9e48f2d4f31a2
jhShin <sinmagic123@gmail.com>
2024-12-05 10:14:17 +0900

    commit

---
+++ spring_mvc3/src/test/java/com/example/spring_mvc3/SpringMvc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
